b165d8c8260a6f50cab96528d1d9c01d9827a5df
Yehan Pathiratne <yehanpathirathne@gmail.com>
2026-08-17 20:12:09 +0530

    Create Task 1 folder and Main.java file

---
+++ Task1/Main.java
@@ -0,0 +1,17 @@
+public class Main{
+    public static void main(String[] args){
+
+        // THIS IS MY FIRST JAVA PROGRAM.
+
+        /*This
+        is
+        a
+        multi-line
+        comment
+        */
+
+        System.out.println("I like pizza!");
+        System.out.print("It's really taste.\n");
+        System.out.println("Buy me pizza!");
+    }
+}
