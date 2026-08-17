2169d7d2f2513e19c0de92abb7619a6fbd47db75
Yehan Pathiratne <yehanpathirathne@gmail.com>
2026-08-17 18:00:40 +0530

    Add comments
    Learned about how to write comment single and multiple.

--- MyFirstproject/Main.java
+++ MyFirstproject/Main.java
@@ -1,7 +1,18 @@
 public class Main{
     public static void main(String[] args){
-        System.out.print("I like pizza!");
-        System.out.println("It's really taste.");
-        
+
+        // THIS IS MY FIRST JAVA PROGRAM.
+
+        /*This
+        is
+        a
+        multi-line
+        comment
+        */
+
+        System.out.println("I like pizza!");
+        System.out.print("It's really taste.\n");
+        System.out.println("Buy me pizza!");
+
     }
 }
