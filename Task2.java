4572230fb6d7c2c63efed1137e83233fe4609a63
Yehan Pathiratne <yehanpathirathne@gmail.com>
2026-08-17 21:47:52 +0530

    Add Operators

--- Task2.java
+++ Task2.java
@@ -5,7 +5,7 @@ public class Task2{
     
     int age;    // int variable
     age = 19;
-    System.out.print(age);
+    System.out.println(age);
     
     double GPA = 3.75;  // double variable
 
@@ -16,5 +16,15 @@ public class Task2{
     String name = "Yehan";  // name variable
 
     long largePopulation = 80_000_000L;
+
+    // operaters
+    int a = 21;
+    int b = 5;
+
+    System.out.println("a + b = "+(a+b));
+    System.out.println("a - b = "+(a-b));
+    System.out.println("a * b = "+(a*b));
+    System.out.println("a ÷ b = "+(a/b));
+    System.out.println("a % b = "+(a%b));
     }
 }
