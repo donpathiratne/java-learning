408ae3e2fb59ef8bf35913d753622c53ce9655db
Yehan Pathiratne <yehanpathirathne@gmail.com>
2026-08-22 13:44:23 +0530

    Add conditi0na part

---
+++ Task3.java
@@ -0,0 +1,22 @@
+public class Task3{
+    public static void main(String[] args){
+
+    // conditional statements
+    // if, else if, else
+
+    double marks = 72;
+
+    if (marks >= 75){
+        System.out.println("Distinction");
+    }
+    else if(marks >= 65){
+        System.out.println("Credit");
+    }
+    else if(marks >= 50){
+        System.out.println("Pass");
+    } 
+    else{
+        System.out.println("Fail");
+    }
+    }
+}
